6959c744d347cef184bdda227e9216bef7be0c47
Rahul Soni <rahul@theredshift.org>
2022-04-01 21:06:10 +0100

    added SO answer attribution

--- problem10.py
+++ problem10.py
@@ -1,7 +1,7 @@
 """
 Ok, so my first try for this one was re-using the code from problem 7, but that's clearly going to take ages. While looking into ways of generating primes, I realised that it's way easier to use a sieve (i.e. generate a list 1..N and then remove things) that only leaves primes than it is to actually generate them directly. Since I know we don't want any primes above 2,000,000, I can generate a collection of the integers 1..2,000,000 and then use a sieve. There are some more modern (and usually faster) options but they're less intuitive, so I'm going with Eratosthenes' sieve (https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes), since he's based. 
 
-Ok, came back to this later, gave up and decided to just use numpy, and honestly I'd forgotten how awesome it is. This runs in 0.0086379 seconds, which is just insane.
+Ok, came back to this later, gave up and decided to just use numpy, and honestly I'd forgotten how awesome it is. Below is adapated from https://stackoverflow.com/questions/3939660/sieve-of-eratosthenes-finding-primes-python. This runs in 0.0086379 seconds, which is just insane.
 
 """
 from time import perf_counter 
